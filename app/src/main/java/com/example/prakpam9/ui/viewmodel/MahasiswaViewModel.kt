a6f02c88256802c53e1ef7293a4853ebecd574cf
mutiara126 <salsamutiara1812@gmail.com>
2024-12-16 01:35:55 +0700

    untuk validasi

--- app/src/main/java/com/example/prakpam9/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/prakpam9/ui/viewmodel/MahasiswaViewModel.kt
@@ -20,5 +20,90 @@ class MahasiswaViewModel(private val repositoryMhs: RepositoryMhs): ViewModel() 
         )
     }
 
-   
+    //validasi data input pengguna
+    private fun validateFields(): Boolean {
+        var event = uiState.mahasiswaEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    //menyimpan data ke repository
+    fun savedata() {
+        val currentEvent = uiState.mahasiswaEvent
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryMhs.insertMhs(currentEvent.toMahasiswaEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        mahasiswaEvent = MahasiswaEvent(), //Reset input form
+                        isEntryValid = FormErrorState() //Reset error state
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage  = "Data Gagal Disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data anda"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage() {
+        TODO("Not yet implemented")
+    }
+
+    fun saveData() {
+        TODO("Not yet implemented")
+    }
+}
+data class MhsUIState(
+    val mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+ data class FormErrorState(
+     val nim: String? = null,
+     val nama: String? = null,
+     val jenisKelamin: String? = null,
+     val alamat: String? = null,
+     val kelas: String? = null,
+     val angkatan: String? = null
+ ){
+     fun isValid(): Boolean {
+         return nim == null && nama == null && jenisKelamin == null &&
+                 alamat == null && kelas == null && angkatan == null
+     }
+ }
+    //Menyimpan input form ke dalam entity
+    fun MahasiswaEvent.toMahasiswaEntity(): Mahasiswa = Mahasiswa(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan,
+    )
+    //data class variabel yg menyimpan data input form
+    data class MahasiswaEvent(
+        val nim: String = "",
+        val nama : String = "",
+        val jenisKelamin : String = "",
+        val alamat: String = "",
+        val kelas: String = "",
+        val angkatan: String = "",
+
+    )
+
 
