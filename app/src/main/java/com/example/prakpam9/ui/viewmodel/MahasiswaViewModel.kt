c2bd3ae28f18ade90e0181a42759b301ce2ec159
mutiara126 <salsamutiara1812@gmail.com>
2024-12-16 01:36:54 +0700

    untuk menyimpan data ke repository

--- app/src/main/java/com/example/prakpam9/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/prakpam9/ui/viewmodel/MahasiswaViewModel.kt
@@ -67,43 +67,5 @@ class MahasiswaViewModel(private val repositoryMhs: RepositoryMhs): ViewModel() 
         TODO("Not yet implemented")
     }
 }
-data class MhsUIState(
-    val mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
-    val isEntryValid: FormErrorState = FormErrorState(),
-    val snackBarMessage: String? = null,
-)
-
- data class FormErrorState(
-     val nim: String? = null,
-     val nama: String? = null,
-     val jenisKelamin: String? = null,
-     val alamat: String? = null,
-     val kelas: String? = null,
-     val angkatan: String? = null
- ){
-     fun isValid(): Boolean {
-         return nim == null && nama == null && jenisKelamin == null &&
-                 alamat == null && kelas == null && angkatan == null
-     }
- }
-    //Menyimpan input form ke dalam entity
-    fun MahasiswaEvent.toMahasiswaEntity(): Mahasiswa = Mahasiswa(
-        nim = nim,
-        nama = nama,
-        jenisKelamin = jenisKelamin,
-        alamat = alamat,
-        kelas = kelas,
-        angkatan = angkatan,
-    )
-    //data class variabel yg menyimpan data input form
-    data class MahasiswaEvent(
-        val nim: String = "",
-        val nama : String = "",
-        val jenisKelamin : String = "",
-        val alamat: String = "",
-        val kelas: String = "",
-        val angkatan: String = "",
-
-    )
 
 
